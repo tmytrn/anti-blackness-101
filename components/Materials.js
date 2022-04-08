c725b1f59baed6f1f8984fc7b9c5dcf6a00519a0
Tommy Tran <tommy@tmytrn.com>
2022-04-08 15:08:43 -0500

    initial commit

---
+++ components/Materials.js
@@ -0,0 +1,39 @@
+import styles from "../styles/Home.module.scss";
+
+const Materials = ({ materials }) => {
+  return (
+    <>
+      <h2 className={styles.heading_h2}>Materials</h2>
+      {materials.map((w, key) => {
+        return (
+          <div className={styles.week_section} key={key}>
+            <h2 className={styles.week_header}>{w.week}</h2>
+            {w.materials.map((material, key) => {
+              if (material.type === "reading") {
+                return (
+                  <div className={styles.material} key={key}>
+                    <a className={styles.material_name} href={material.link}>
+                      {material.title}
+                    </a>
+                    <p className={styles.material_type}>PDF</p>
+                  </div>
+                );
+              } else {
+                return (
+                  <div className={styles.material} key={key}>
+                    <a className={styles.material_name} href={material.link}>
+                      {material.title}
+                    </a>
+                    <p className={styles.material_type}>VIDEO</p>
+                  </div>
+                );
+              }
+            })}
+          </div>
+        );
+      })}
+    </>
+  );
+};
+
+export default Materials;
---
+++ pages/index.js
@@ -0,0 +1,56 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.scss";
+import { data } from "../materials";
+import Materials from "../components/Materials";
+
+export const getStaticProps = async () => {
+  return {
+    props: {
+      materials: data,
+    },
+  };
+};
+
+export default function Home({ materials }) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Anti Blackness 101</title>
+        <meta name="description" content="Anti Blackness 101" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>Anti Blackness 101</h1>
+        <Materials materials={materials} />
+        <div className={styles.reflection}>
+          <h3 className={styles.heading_h3}>My Reflection</h3>
+          <p>
+            This class has completely reshaped how I think about racism and
+            colorism in our society. Race as we know it today has been invented
+            to oppress people with darker skin, and to enable prejudice. Our
+            cities have been built to segregate us and keep us divided. Many
+            laws were created to target racial minorities and to empower white
+            majorities.
+          </p>
+          <p>
+            We can't reach a post-racial society by ignoring the color of
+            people's skin. We have to talk about it, acknowledge the history,
+            and find ways to undo the structures built on racial prejudice and
+            discrmination.
+          </p>
+          <p>
+            I hope to continue my journey of unlearning anti-blackness by
+            creating resources to help educate people and to help them
+          </p>
+        </div>
+      </main>
+
+      <footer className={styles.footer}>
+        This site was made as a class project for{" "}
+        <a href="https://www.peoplematter.one/">People Matter</a>
+      </footer>
+    </div>
+  );
+}
